150fe9e4e0ecc979042b93f88769a98c3ce27505
REDANE14 <101236149+REDANE14@users.noreply.github.com>
2022-03-09 12:00:46 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,40 @@
+var form, todoList, button;
+
+
+form = document.querySelector('form');
+
+todoList = document.querySelector('ul');
+
+button = document.getElementById('clear');
+
+var input = document.getElementById('user-todo');
+
+document.getElementById('clear').disabled = true;
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    todoMaker(input.value);
+    input.value = ' ';
+
+})
+
+var todoMaker = function(text) {
+
+    let div = document.createElement('div');
+    div.classList.add('text');
+    var todo = document.createElement('li');
+
+    todo.textContent = text;
+
+    todoList.appendChild(todo)
+    todo.style.listStyleType = "none";
+
+}
+
+button.addEventListener('click', function() {
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.firstChild)
+        document.getElementById('clear').disabled = true;
+    }
+});
